8a7a0cd9ba0213abef1972e67f6fb211d4deec78
JaredMel <jareddmelendez1764@gmail.com>
2024-08-27 17:27:43 -0700

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -18,3 +18,58 @@ for (let skill of skillsList)
     skillItem.innerHTML = skill;
     skillsUL.appendChild(skillItem);
 }
+
+let messageForm = document.querySelector("[name='leave_message']");
+let messageSection = document.getElementById('message-section');
+let messageList = messageSection.querySelector('ul');
+messageSection.hidden = true;
+
+let idCounter = 0;
+//create unique id's for entries
+//closure on idCounter
+function makeId() {
+    let id = 'entry' + idCounter++;
+    return id;
+}
+
+let entryById={};
+
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let name = event.target.usersName.value;
+    let email = event.target.usersEmail.value;
+    let message = event.target.usersMessage.value;
+
+    console.log('Name: ' , name);
+    console.log('Email: ', email);
+    console.log('Message:', message);
+    let uid = makeId();
+    let newMessage = document.createElement('li');
+    newMessage.classList.add('message-item');
+
+    newMessage.innerHTML = `<a href="mailto:${email} ">${name} </a><span>wrote: ${message} </span>`;
+    newMessage.setAttribute('id', uid);
+
+    newMessage.appendChild(makeRemoveButton());
+    messageList.appendChild(newMessage);
+    messageForm.reset();
+    messageSection.hidden = false;
+});
+
+function makeRemoveButton()
+{
+    let removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    removeButton.className = 'remove-button';
+    removeButton.addEventListener('click', () => {
+        let entry = removeButton.parentNode;
+        let uid1 = entry.getAttribute('id');
+        delete entryById[uid1];
+        entry.remove();
+        if (messageList.childElementCount === 0) {
+            messageSection.hidden = true;
+        };
+    });
+    return removeButton;
+};
